568cb7bb4bbec374616c46931a36ecea6f9e77ca
nbush <nbush@rochester.k12.mi.us>
2023-12-28 13:40:15 -0500

    Fixed DCU not being detected via regkey

--- .modules/Get-DellUpdate.psm1
+++ .modules/Get-DellUpdate.psm1
@@ -8,17 +8,9 @@ function Get-DellUpdate
     $current_function = $MyInvocation.InvocationName
     Write-Log -String "Begin function $current_function."
 
-    # Older 64-bit version of Dell Command Update
-    # $dcu_regkey = "{5EBBC1DA-975F-44A0-B438-F325BCD45577}"
-
-    # Newer 32-bit version of Dell Command Update (Jul 2018)
-    # $dcu_regkey = "{289899DB-189C-4B8E-A160-C6D146B5C74A}"
-
     # Newer 32-bit version of Dell Command Update (Oct 2023)
-    # $dcu_regkey = "{612F7720-D28A-473F-8FB9-C8D300B5F534}"
-    $dcu_regkey = "0277F216A82DF374F89B8C3D005B5F43"
-
-    $dcu_regpath = "HKLM:\SOFTWARE\Classes\Installer\Products\$dcu_regkey"
+    $dcu_regkey = "{612F7720-D28A-473F-8FB9-C8D300B5F534}"
+    $dcu_regpath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\$dcu_regkey"
 
     Write-Log -String "     Registry key for DCU (hardcoded): $dcu_regkey"
     
