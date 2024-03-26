ecdeb909f6c65258b2ea3e262a425eadb8c2e420
nbush <nbush@rochester.k12.mi.us>
2024-03-26 09:43:14 -0400

    Implemented list and hashtable printing to log

--- .modules/Write-Log.psm1
+++ .modules/Write-Log.psm1
@@ -2,39 +2,43 @@ function Write-Log
 {
     Param
     (
-        [Parameter(Mandatory=$false,
-                   Position=0)]
-        [ValidateNotNull()]
         $string,
-
-        [Parameter(Mandatory=$false,
-                   Position=0)]
-        [ValidateNotNull()]
-        $hashtable
+        $list,
+        $hashtable,
+        $logflag
     )
 
-    $wuplog = "$env:TEMP\wup.log"
-    $getdate = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-
-    if ($string){"$getdate   $string" | Out-File -FilePath $wuplog -Append}
-    elseif ($hashtable)
+    if ($logflag)
     {
-        $hashtable.GetEnumerator() | ForEach-Object{
+        $wuplog = "$env:TEMP\wup.log"
+        $getdate = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+        $indent = "                                   "
 
-            if (!($_.Key -like "*string*")) {
-                $logstring = "         " + $_.Key + ': ' + $_.Value.trim()
-            }
+        if  ($string)   {"$getdate   $string" | Out-File -FilePath $wuplog -Append}
+        elseif ($list)  {$list | ForEach-Object {$indent + $_ | Out-File -FilePath $wuplog -Append}}
+        elseif ($hashtable)
+        {
+            $hashtable.GetEnumerator() | ForEach-Object {
 
-            # Remove unicode check/x strings
-            if ($_.Value -like "*33m*" -or $_.Value -like "*37m*") {
-                $tempstring = $_.Value.split(' ')
-                $first, $rest= $tempstring
-                $logstring = "         " + $_.Key + ': ' + $rest.trim()
-            }
+                # Strip unicode check/x strings from *_string variables (won't translate correctly in logs)
+                if ($_.Value -like "*33m*" -or $_.Value -like "*37m*") 
+                {
+                    $tempstring = $_.Value.split(' ')
+                    $first, $rest= $tempstring
+                    $logstring = $indent + $_.Key + ': ' + $rest.trim()
+                }
+                
+                # For everything else
+                else
+                {
+                    $logstring = $indent + $_.Key + ': ' + $_.Value.trim()
+                }
 
-            # Remove blank k/v pair (used for clean host printout)
-            if (!($logstring.trim() -eq ":")){
-                "$getdate   $logstring" | Out-File -FilePath $wuplog -Append
+                # Remove blank k/v pairs (used for clean host printout)
+                if (!($logstring.trim() -eq ":"))
+                {
+                    "   $logstring" | Out-File -FilePath $wuplog -Append
+                }
             }
         }
     }
